996e3636d0ff44357f13965bd8bb4b934f9fc738
Guilherme Mertens <gvmertens@gmail.com>
2020-01-28 14:26:25 -0300

    fim do crud

--- index.js
+++ index.js
@@ -2,7 +2,42 @@ const express = require('express');
 
 const server = express();
 
-const users = ['Diego', 'Guilherme', 'Vitor'];
+server.use(express.json());
+
+const users = ['Renata', 'Guilherme', 'Vitor'];
+
+server.get('/users', (req, res) => {
+    return res.json(users);
+});
+
+server.post('/users', (req, res) => {
+
+    const { nome } = req.body;
+
+    users.push(nome);
+
+    return res.json(users);
+
+});
+
+server.put('/users/:id', (req, res) =>{
+
+    const { id } = req.params;
+    const { nome } = req.body;
+
+    users[id] = nome;
+
+    return res.json(users);
+});
+
+server.delete('/users/:id', (req, res) =>{
+
+    const { id } = req.params;
+    
+    users.splice(id, 1);
+    
+    return res.send();
+});
 
 server.get('/users/:id', (req, res) =>{
 
