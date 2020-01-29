9735d4d88ea36dbad915fda3f2d36ed3350a27d4
Guilherme Mertens <gvmertens@gmail.com>
2020-01-29 13:02:23 -0300

    fim da tarefa2

--- index.js
+++ index.js
@@ -4,13 +4,43 @@ const server = express();
 
 server.use(express.json());
 
-const users = ['Renata', 'Guilherme', 'Vitor'];
+const users = ['Renata', 'Guilherme', 'Isadora'];
+
+server.use((req, res, next) => {
+
+    console.log(`Método ${req.method}; URL: ${req.url}`);
+    console.time('req')
+
+    next();
+
+    console.timeEnd('req');
+});
+
+function checkUserExists(req, res, next){
+    if(!req.body.nome){
+        return res.status(400).json({ error: 'O campo nome é obrigatório'});
+    }
+
+    return next();
+}
+
+function checkUserInArrey(req, res, next){
+    const user = users[req.params.id];
+    
+    if(!user){
+        return res.status(400).json({ error: 'Usuário não encontrado'});
+    }
+
+    req.user = user;
+
+    return next();
+}
 
 server.get('/users', (req, res) => {
     return res.json(users);
 });
 
-server.post('/users', (req, res) => {
+server.post('/users', checkUserExists, (req, res) => {
 
     const { nome } = req.body;
 
@@ -20,7 +50,7 @@ server.post('/users', (req, res) => {
 
 });
 
-server.put('/users/:id', (req, res) =>{
+server.put('/users/:id', checkUserExists, checkUserInArrey, (req, res) =>{
 
     const { id } = req.params;
     const { nome } = req.body;
@@ -30,7 +60,7 @@ server.put('/users/:id', (req, res) =>{
     return res.json(users);
 });
 
-server.delete('/users/:id', (req, res) =>{
+server.delete('/users/:id', checkUserInArrey, (req, res) =>{
 
     const { id } = req.params;
     
@@ -39,11 +69,9 @@ server.delete('/users/:id', (req, res) =>{
     return res.send();
 });
 
-server.get('/users/:id', (req, res) =>{
+server.get('/users/:id', checkUserInArrey, (req, res) =>{
 
-    const { id } = req.params;
-
-    res.json( users[id]);
+    res.json( req.user);
 });
 
 server.listen(3000);
